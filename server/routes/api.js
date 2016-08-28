9816eebc90167c11443cef6e2b9bbaddf76a2759
kwosei <kwosei@users.noreply.github.com>
2016-08-28 18:43:09 -0400

    remove Trulia api

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,53 +4,72 @@ var express = require('express');
 var router = express.Router();
 var http = require('http');
 var cheerio = require('cheerio');
+var parseString = require('xml2js').parseString;
+var util = require('util')
+//var jsdom = require("jsdom");
 
 router.get('/getZestimate', function (req, res) {
-    console.log(req.query);
-    getZpid(req.query.address, req.query.citystatezip);
-
+    getZestimate(req.query.address, req.query.citystatezip, res);
 })
 
 
-function getZpid(address, citystatezip) {
+function getZestimate(address, citystatezip, res) {
     var zwsid = "X1-ZWz1fetklge1vv_3wguv"
-
     var options = {
         host: "www.zillow.com",
         path: encodeURI("/webservice/GetSearchResults.htm?zws-id=" + zwsid + "&address=" + address + "&citystatezip=" + citystatezip)
 
     }
-
     var callback = function (response) {
         var str = '';
-
-        //another chunk of data has been recieved, so append it to `str`
         response.on('data', function (chunk) {
             str += chunk;
         });
-
-        //the whole response has been recieved, so we just print it out here
         response.on('end', function () {
-            //console.log(str);
-            var xml = cheerio.load(str, {xmlMode: true});
-            var zestimate = cheerio.load(xml('zestimate').html());
-            var amount = zestimate('amount').html();
-            var lastupdated = zestimate('last-updated').html();
-            var valuationRange = cheerio.load(zestimate('valuationRange').html());
-            var valuationMax = valuationRange('high').html();
-            var valuationMin = valuationRange('low').html();
-            console.log("amount is", amount, "lastupdate is", lastupdated, "valMax is", valuationMax, "valMin is", valuationMin);
+            if (str.length === 0) {
+                res.status(200).send("no data available");
+                return;
+            }
+
+            var xml = cheerio.load(str, { xmlMode: true });
+
+
+            /* ERROR handling */
+            var responseCode = xml('code').html();
+
+            if (responseCode !== "0") {
+                console.log("ERROR** no data found")
+                res.json({errorMessage: "No data found for this address"});
+                return;
+            }
+
+
+            
+            var zestimateNode = xml('zestimate').html();
+            var zestimate, amount, lastupdated, valuationRange, valuationMax, valuationMin;
+            if (!(zestimateNode === null)) {
+                zestimate = cheerio.load(zestimateNode);
+                amount = zestimate('amount').html();
+                lastupdated = zestimate('last-updated').html();
+                valuationRange = cheerio.load(zestimate('valuationRange').html());
+                valuationMax = valuationRange('high').html();
+                valuationMin = valuationRange('low').html();
+            }
+
+
+
+            console.log("***ZILLOW*** \n", "amount is", amount, "lastupdate is", lastupdated, "valMax is", valuationMax, "valMin is", valuationMin);
+            res.status(200).json({ amout: amount, lastupdated: lastupdated, valuationMax: valuationMax, valuationMin: valuationMin })
         });
     }
-
-
     http.request(options, callback).end();
-
-
-    // var baseUrl = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=" + zwsid + "&address=" + address + "&citystatezip=" + citystatezip;
-
-
 }
 
 
+
+
+
+
 module.exports = router;
+
+
