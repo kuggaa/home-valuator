90310d9d3e10c26061d0cab8dec29d2cfd160b05
kwosei <kwosei@users.noreply.github.com>
2016-08-29 23:07:53 -0400

    remove views set

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ var port = process.env.PORT || 3000;
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
